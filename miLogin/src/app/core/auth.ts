6632accbb5c31acc41609bde29f6ce112d04ac00
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-10-28 17:36:15 -0500

    update clases

--- miLogin/src/app/core/auth.ts
+++ miLogin/src/app/core/auth.ts
@@ -4,7 +4,9 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private afAuth: AngularFireAuth) {}
-
+  register(email:string, password:string){
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
   login(email: string, password: string) {
     return this.afAuth.signInWithEmailAndPassword(email, password);
   }
