1c7994e331e9f7658d1756152e21c861da322b85
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-10-28 17:20:22 -0500

    UPDATE CLASE 28-10 AUTHENTICATION FIREBASE

--- pokeApp/src/app/services/pokeapi.ts
+++ pokeApp/src/app/services/pokeapi.ts
@@ -12,11 +12,7 @@ export class Pokeapi {
   constructor(private http:HttpClient){ }
 
   //Obtener la informacion de la API
-<<<<<<< HEAD
-  getPokemonList(limit: number= 10, offset: number=0): Observable<any>{
-=======
   getPokemonList(limit: number= 10, offset:number=0): Observable<any>{
->>>>>>> 0351f4b02baaf9bb5b10bdf0d6ac032c6cf70098
     return this.http.get<any>(`${this.API_URL}pokemon?limit=${limit}&offset=${offset}`).pipe(
       switchMap(response=> {
         const pokemonDetails$= response.results.map((pokemon: any) => this.http.get(pokemon.url));
