bb4379ad6b22385858653e30bea2263bd859e33c
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-10-28 17:43:31 -0500

    UPDATE CLASE

--- miLogin/src/app/pages/register/register.page.ts
+++ miLogin/src/app/pages/register/register.page.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/core/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: false,
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  email:string='';
+  password:string='';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router:Router){
 
-  ngOnInit() {
+  }
+
+  register(){
+     if (!this.email || !this.password) {
+      alert('Por favor ingresa un correo y contraseña.');
+      return;
+    }
+    try {
+      const userCredential = this.authService.register(this.email, this.password);
+      console.log('Usuario registrado:', userCredential);
+      alert('Usuario registrado correctamente 🎉');
+      this.router.navigate(['/login']); // redirige a la página de login
+    } catch (error) {
+      
+    }
   }
 
 }
