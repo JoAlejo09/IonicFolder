b5049dec67db0be94b0f1efd3c9fa74d0bff85fe
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-11-10 16:56:58 -0500

    UPDATE

--- miLoginSupa/src/environments/environment.prod.ts
+++ miLoginSupa/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseURL:'https://qhmjhvmxvjbtazkeynod.supabase.co',
+  supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFobWpodm14dmpidGF6a2V5bm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTE5MDYsImV4cCI6MjA3ODM2NzkwNn0.Ls9dK9ohuA5nrsAeNl7R8qkcAgNw7_zb70aa7H4u55c'
+
 };
--- miLoginSupa/src/environments/environment.ts
+++ miLoginSupa/src/environments/environment.ts
@@ -4,12 +4,9 @@
 
 export const environment = {
   production: false,
-  /*
   supabaseURL:'https://qhmjhvmxvjbtazkeynod.supabase.co',
   supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFobWpodm14dmpidGF6a2V5bm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTE5MDYsImV4cCI6MjA3ODM2NzkwNn0.Ls9dK9ohuA5nrsAeNl7R8qkcAgNw7_zb70aa7H4u55c'
-*/
-  supabaseURL: 'https://lsmcqjdofuiluvpnwenz.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzbWNxamRvZnVpbHV2cG53ZW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTQ5ODksImV4cCI6MjA3ODM3MDk4OX0.VH19TmLfdLRB9sTDLAiz_FAKfPzoodDIiVP18Cj1VZc'
+
 };
 
 /*
